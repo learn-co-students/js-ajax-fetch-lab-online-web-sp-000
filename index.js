331c16536051cbfd90341c62da4d178bb659ca61
Jonathan Tuzman <tuzmusic@gmail.com>
2019-02-20 10:34:25 -0500

    implement forkRepo, showResults

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
+// REMEMBER TO COMMIT WHEN ADDING SOMETHING THAT WORKS!
+
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return '';
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
+  const repo = "learn-co-curriculum/js-ajax-fetch-lab";
+  // POST /repos/:owner/:repo/forks
+  fetch(`https://api.github.com/repos/${repo}/forks`, { 
+    method: "POST",
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+    .then(res => res.json())
+    .then(json => showResults(json));
   //use fetch to fork it!
 }
 
 function showResults(json) {
   //use this function to display the results from forking via the API
+  const url = json.html_url
+  const link = `<a href="${url}">Your Repo</a>`
+  document.getElementById('results').innerHTML = link
 }
 
 function createIssue() {
   //use this function to create an issue based on the values input in index.html
+  const title = document.getElementById('title').innerHTML
+  const text = document.getElementById('body').innerHTML
 }
 
 function getIssues() {
