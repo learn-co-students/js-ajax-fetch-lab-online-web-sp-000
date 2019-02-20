21962ee4e1056b38db1d3d58823b1cac018fb19d
Jonathan Tuzman <tuzmusic@gmail.com>
2019-02-20 10:56:54 -0500

    Done.

--- index.js
+++ index.js
@@ -3,12 +3,14 @@
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
+  return ''
 }
+const learnOwner = "learn-co-curriculum"
+const repo = "js-ajax-fetch-lab";
 
 function forkRepo() {
-  const repo = "learn-co-curriculum/js-ajax-fetch-lab";
   // POST /repos/:owner/:repo/forks
-  fetch(`https://api.github.com/repos/${repo}/forks`, { 
+  fetch(`https://api.github.com/repos/${learnOwner}/${repo}/forks`, { 
     method: "POST",
     headers: {
       Authorization: `token ${getToken()}`
@@ -28,10 +30,32 @@ function showResults(json) {
 
 function createIssue() {
   //use this function to create an issue based on the values input in index.html
-  const title = document.getElementById('title').innerHTML
-  const text = document.getElementById('body').innerHTML
+  const title = document.getElementById('title').value
+  const text = document.getElementById('body').value
+  fetch(`https://api.github.com/repos/tuzmusic/${repo}/issues`, {
+    method: "POST",
+    title: title,
+    body: text,
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+    .then(response=>
+      getIssues()
+      )
 }
 
 function getIssues() {
   //once an issue is submitted, fetch all open issues to see the issues you are creating
+  fetch(`https://api.github.com/repos/tuzmusic/${repo}/issues`, {
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+    .then(res => res.json())
+    .then(json => showIssues(json));
+}
+
+function showIssues(json) {
+  debugger
 }
