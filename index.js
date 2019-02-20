ef87cf7c0e0266c05a1971a307bc1eb54477edc5
Jonathan Tuzman <tuzmusic@gmail.com>
2019-02-20 11:27:24 -0500

    Done.

--- index.js
+++ index.js
@@ -3,14 +3,14 @@
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return ''
+  return "";
 }
-const learnOwner = "learn-co-curriculum"
+const learnOwner = "learn-co-curriculum";
 const repo = "js-ajax-fetch-lab";
 
 function forkRepo() {
   // POST /repos/:owner/:repo/forks
-  fetch(`https://api.github.com/repos/${learnOwner}/${repo}/forks`, { 
+  fetch(`https://api.github.com/repos/${learnOwner}/${repo}/forks`, {
     method: "POST",
     headers: {
       Authorization: `token ${getToken()}`
@@ -23,30 +23,28 @@ function forkRepo() {
 
 function showResults(json) {
   //use this function to display the results from forking via the API
-  const url = json.html_url
-  const link = `<a href="${url}">Your Repo</a>`
-  document.getElementById('results').innerHTML = link
+  const url = json.html_url;
+  const link = `<a href="${url}">Your Repo</a>`;
+  document.getElementById("results").innerHTML = link;
 }
 
 function createIssue() {
-  //use this function to create an issue based on the values input in index.html
-  const title = document.getElementById('title').value
-  const text = document.getElementById('body').value
+  // POST /repos/:owner/:repo/issues
+  const title = document.getElementById("title").value;
+  const text = document.getElementById("body").value;
+  const body = JSON.stringify({ title: title, body: text })
   fetch(`https://api.github.com/repos/tuzmusic/${repo}/issues`, {
     method: "POST",
-    title: title,
-    body: text,
+    body: body,
     headers: {
       Authorization: `token ${getToken()}`
     }
   })
-    .then(response=>
-      getIssues()
-      )
+    .then(response => getIssues())
+    .catch(error => console.log(error));
 }
 
 function getIssues() {
-  //once an issue is submitted, fetch all open issues to see the issues you are creating
   fetch(`https://api.github.com/repos/tuzmusic/${repo}/issues`, {
     headers: {
       Authorization: `token ${getToken()}`
@@ -57,5 +55,9 @@ function getIssues() {
 }
 
 function showIssues(json) {
-  debugger
+  let str = '<ul>'
+  json.forEach(issue => {
+    str += `<li><a href="${issue.html_url}">${issue.title}</a></li>`
+  });
+  document.getElementById('issues').innerHTML = str + '</ul>'
 }
