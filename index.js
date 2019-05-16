36a2b02d820375ede4f99b51f0d8975c5504bd58
github username <github email address>
2019-05-15 23:40:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,67 @@
 function getToken() {
-  //change to your token to run in browser, but set
-  //back to '' before committing so all tests pass
   return '';
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-  //use fetch to fork it!
+  fetch(`https://api.github.com/repos/${repo}/forks`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `token ${getToken()}`
+      }
+    })
+      .then(res => res.json())
+      .then(json => showResults(json));
 }
 
 function showResults(json) {
-  //use this function to display the results from forking via the API
+  document.getElementById("results").innerHTML = `<a href=${json.html_url}>${json.html_url}</a>`;
 }
 
 function createIssue() {
-  //use this function to create an issue based on the values input in index.html
+  const repo = `cookiemccormick/js-ajax-fetch-lab`;
+  const postData = {
+    title: document.getElementById("title").value,
+    body: document.getElementById("body").value
+  };
+  fetch(`https://api.github.com/repos/${repo}/issues`,
+    {
+      method: 'POST',
+      body: JSON.stringify(postData),
+      headers: {
+        Authorization: `token ${getToken()}`
+      }
+    })
+    .then(res => res.json())
+    .then(json => getIssues(json));
 }
 
-function getIssues() {
-  //once an issue is submitted, fetch all open issues to see the issues you are creating
+function getIssues(json) {
+  const repo = `cookiemccormick/js-ajax-fetch-lab`;
+  fetch(`https://api.github.com/repos/${repo}/issues?state=open`,
+    {
+      method: 'GET',
+      headers: {
+        Authorization: `token ${getToken()}`
+      }
+    })
+    .then(res => res.json())
+    .then(json => showIssues(json));
+}
+
+function showIssues(issues) {
+  let issuesList = "<ul>";
+
+  for (var i = 0; i < issues.length; i++) {
+    const issue = issues[i];
+    issuesList += `
+      <li>
+        ${issue.title} - ${issue.body}
+      </li>`;
+  }
+
+  issuesList += "</ul>";
+
+  document.getElementById("issues").innerHTML = issuesList;
 }
