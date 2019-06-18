b8b148e2474daefc37d134e0ee5ad2a479440b0e
github username <github email address>
2019-06-18 12:41:01 -0400

    Done.

--- index.js
+++ index.js
@@ -10,14 +10,39 @@ function getToken() {
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
   //use fetch to fork it!
+  fetch(`https://api.github.com/repos/${repo}/forks`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `token ${getToken()}`
+      }
+    })
+      .then(res => res.json())
+      .then(json => showResults(json));
 }
 
 function showResults(json) {
   //use this function to display the results from forking via the API
+   document.getElementById("results").innerHTML = `<a href=${json.html_url}>${json.html_url}</a>`;
 }
 
 function createIssue() {
   //use this function to create an issue based on the values input in index.html
+  const repo = `nicoledag/js-ajax-fetch-lab`;
+  const postData = {
+    title: document.getElementById("title").value,
+    body: document.getElementById("body").value
+  };
+  fetch(`https://api.github.com/repos/${repo}/issues`,
+    {
+      method: 'POST',
+      body: JSON.stringify(postData),
+      headers: {
+        Authorization: `token ${getToken()}`
+      }
+    })
+    .then(res => res.json())
+    .then(json => getIssues(json));
 }
 
 function getIssues() {
