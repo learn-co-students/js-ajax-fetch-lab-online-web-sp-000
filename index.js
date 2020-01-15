23a3d4b6cca83358eb5133b5c1d53d2648c8d940
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-01-15 18:43:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 const baseURL = 'https://api.github.com';
-const user = '<YOUR_USERNAME>';
+const user = 'ChaimSh';
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return '';
+
+  return '851a93c0ae47e161d479207aa413822165075f58';
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
   //use fetch to fork it!
+  const url  = `${baseURL}/repos/${repo}/forks`;
+  const postData = {
+  body: 'Great stuff'
+};
+  fetch(url, {
+    method: 'POST',
+    body:JSON.stringify(postData)
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+
+  })
+  .then(res => res.json())
+  .then(json => console.log(json))
 }
 
 function showResults(json) {
   //use this function to display the results from forking via the API
+document.getElementById('results').innerHTML = `<a href=${json.html_url}>${json.html_url}</a>`
 }
 
 function createIssue() {
   //use this function to create an issue based on the values input in index.html
+
 }
 
 function getIssues() {
