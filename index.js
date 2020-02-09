14a81a0b7d4028689a157ca4e7419e5f7ac61d76
github username <github email address>
2020-02-09 12:50:18 -0500

    Done.

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
   return '';
-  
-  //return '';
 }
 
 function forkRepo() {
@@ -64,62 +62,3 @@ fetch(
 }).then(res => res.json())
   .then(json => document.getElementById('issues').innerHTML = json);
 }
-
-
-
-
-
-
-
-
-
-// function forkRepo() {
-//   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-//   const url = `${baseURL}/repos/${repo}/forks`;
-//   fetch(url, {
-//     method: 'POST',
-//     headers: {
-//       Authorization: `token ${getToken()}`
-//     }
-//   })
-//     .then(res => res.json())
-//     .then(json => showResults(json));
-// }
-//
-// function showResults(json) {
-//   document.getElementById('results').innerHTML = `<a href=${json.html_url}>${
-//     json.html_url
-//   }</a>`;
-// }
-//
-// function createIssue() {
-//   const repo = `${user}/js-ajax-fetch-lab`;
-//   const url = `${baseURL}/repos/${repo}/issues`;
-//   const postData = {
-//     title: document.getElementById('title').value,
-//     body: document.getElementById('body').value
-//   };
-//
-//   fetch(url, {
-//     method: 'POST',
-//     body: JSON.stringify(postData),
-//     headers: {
-//       Authorization: `token ${getToken()}`
-//     }
-//   })
-//     .then(res => res.json())
-//     .then(json => getIssues());
-// }
-//
-// function getIssues() {
-//   const repo = `${user}/js-ajax-fetch-lab`;
-//   const url = `${baseURL}/repos/${repo}/issues`;
-//   fetch(url, {
-//     headers: {
-//       Authorization: `token ${getToken()}`
-//     }
-//   })
-//     .then(res => res.json())
-//     //.then(json => console.log(json));
-//     .then(json => document.getElementById('issues').innerHTML = json);
-// }
